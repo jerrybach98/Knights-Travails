25040e06b67f934e79dea405aeded3590994c194
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2023-09-29 03:21:49 -0700

    dequeue starting position for additional proccessing
    Append to queue possible moves from current position

--- main.rb
+++ main.rb
@@ -2,14 +2,27 @@
 # can build root of tree from perspective knight start
 # each node represents position on chess board
 # potential moves as children to the root > each children has their own children
-# #  BFS you're checking all possible 1-long paths then if no solution, all possible 2-long paths then all 3-long then all 4-long etc.
-# You take a path, check if the end of the path is the end position. if not de-queue the path and add all the paths that the last node in that path lead to?
-# implement a queue with my exisiting code
 # possible moves(routes) add and substract to the desired end code: this in return mirrors a chess board
 # each child has all possible moves 
 # moves can not be over 7 and under 0 
 # saved vertices (nodes) moved on
 # there are an infinite amount of ways to get to a node, save the one that takes the least edges or nodes moved
+
+
+
+
+# BFS you're checking all possible 1-long paths then if no solution, all possible 2-long paths then all 3-long then all 4-long etc.
+# until the queue is empty
+# add start to queue
+# current_move = remove from queue for further analysis 
+# try each possible move on removed queue node
+# add moves to queue with answer between 0  - 7
+# find a way to track this path 
+
+
+# if 1 path doesn't work, clear the queue,
+# path 1 should equal = [2, 1], [1, 2]
+# check for 2 length path and so on
 
 
 
@@ -19,60 +32,52 @@ class Game
   def initialize()
     @possible_moves = [[2, 1], [1, 2], [2, -1], [1, -2], [-2, 1], [-1, 2], [-2, -1], [-1, -2]]
     @root = nil
-    @board_positions = []
+    @queue = []
   end
 
   def build_tree(start, destination, possible_moves)
+    root = Knight.new(start)
+    @queue.push(root.data)
 
     if start == destination
-      return nil
+      return root.data
     end
 
-    if @board_positions.count == 64
-      return nil
-    end
-    
-    root = Knight.new(start)
-    @board_positions.push(root.position)
-
-
-    @possible_moves.each do |move|
-      new_start = [root.position[0] + move[0], root.position[1] + move[1]]
-      if new_start[0].between?(0, 7) == true && new_start[1].between?(0, 7) == true && @board_positions.include?(new_start) == false
-        if new_start == destination
-          @board_positions.push(new_start)
-          root.moves = Knight.new(new_start)
-          break
-        else
-          root.moves = build_tree(new_start, destination, possible_moves)
+    until @queue.empty?
+      p @queue
+      position = @queue.shift
+      @possible_moves.each do |move|
+        new_position = [position[0] + move[0], position[1] + move[1]]
+        if new_position[0].between?(0, 7) == true && new_position[1].between?(0, 7)
+          root.moves = Knight.new(new_position)
+          @queue.push(root.moves.data)
         end
-      end 
+      end
+
+      break if position == destination
     end
 
-    @root = root
-
+    return position
   end
 
 
 
   def knight_moves(start, destination)
-    build_tree(start, destination, @possible_moves)
-    p @board_positions
-    p @root
+    p build_tree(start, destination, @possible_moves)
   end
   
 
 end
 
 class Knight
-attr_accessor :position, :moves
+attr_accessor :data, :moves
 
 
-  def initialize(position)
-    @position = position
+  def initialize(data)
+    @data = data
     @moves = nil
   end
 end
 
 game = Game.new
-game.knight_moves([0,0], [1,2])
+game.knight_moves([0,0], [3,3])
