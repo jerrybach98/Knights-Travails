eb8d069f328920b1adefacf63b8fa11698e4f027
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2023-09-29 01:08:22 -0700

    visit every square similar to knights tour algorithm

--- main.rb
+++ main.rb
@@ -1,24 +1,15 @@
-# build the root, root is kngiht moves input value
-
-
-
 # data structure similar to a binary tree: acyclic, undirected graph
-# can build tree from perspective knight and some from board
-# starting point of knight is root of tree
-
-
+# can build root of tree from perspective knight start
 # each node represents position on chess board
 # potential moves as children to the root > each children has their own children
-# used a breadth first approach for shortest path
+# #  BFS you're checking all possible 1-long paths then if no solution, all possible 2-long paths then all 3-long then all 4-long etc.
+# You take a path, check if the end of the path is the end position. if not de-queue the path and add all the paths that the last node in that path lead to?
+# implement a queue with my exisiting code
+# possible moves(routes) add and substract to the desired end code: this in return mirrors a chess board
+# each child has all possible moves 
+# moves can not be over 7 and under 0 
 # saved vertices (nodes) moved on
 # there are an infinite amount of ways to get to a node, save the one that takes the least edges or nodes moved
-
-# possible moves(routes) add and substract to the desired end code: this in return mirrors a chess board (leetcode algorithm)
-# each child has all possible moves 
-
-# moves can not be over 7 and under 0 
-# implement possible moves as children if they don't bring x and y under 0 or over 7
-
 
 
 
@@ -26,38 +17,48 @@ class Game
   attr_accessor :root
 
   def initialize()
-    possible_moves = [[2, 1], [1, 2], [2, -1], [1, -2], [-2, 1], [-1, 2], [-2, -1], [-1, -2]]
-    @root = build_tree(possible_moves)
+    @possible_moves = [[2, 1], [1, 2], [2, -1], [1, -2], [-2, 1], [-1, 2], [-2, -1], [-1, -2]]
+    @root = nil
+    @board_positions = []
   end
 
-  def build_tree(start, possible_moves)
-    #return nil if array.length == 0 between != 0 and 7
+  def build_tree(start, destination, possible_moves)
 
-    root = Knight.new(start)
-
-    # for each node, new variable root.moves = build_tree()
-    possible_moves.map {|v| [@root[0] + v[0], @root[1] + v[1]]}
-      
-        
-        
-        start[0] < 0 || start[0] > 8 || start[1] < 0 || start[1] > 8
-        move_results << new_position
-      end
+    if start == destination
+      return nil
     end
 
+    if @board_positions.count == 64
+      return nil
+    end
+    
+    root = Knight.new(start)
+    @board_positions.push(root.position)
 
 
-    root.moves = Knight.new(
-      # 
-      #
-      #
-    )
+    @possible_moves.each do |move|
+      new_start = [root.position[0] + move[0], root.position[1] + move[1]]
+      if new_start[0].between?(0, 7) == true && new_start[1].between?(0, 7) == true && @board_positions.include?(new_start) == false
+        if new_start == destination
+          @board_positions.push(new_start)
+          root.moves = Knight.new(new_start)
+          break
+        else
+          root.moves = build_tree(new_start, destination, possible_moves)
+        end
+      end 
+    end
+
+    @root = root
 
   end
 
 
-  def knight_moves(start, end)
-    build_tree(start, possible_moves)
+
+  def knight_moves(start, destination)
+    build_tree(start, destination, @possible_moves)
+    p @board_positions
+    p @root
   end
   
 
@@ -68,10 +69,10 @@ attr_accessor :position, :moves
 
 
   def initialize(position)
-    @posiion = position
+    @position = position
     @moves = nil
   end
 end
 
 game = Game.new
-#knight_moves([3,3],[4,3])
+game.knight_moves([0,0], [1,2])
